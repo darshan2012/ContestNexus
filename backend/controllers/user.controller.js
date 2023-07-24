9973732c8e98d45da739078267a02ac1ae0d16de
Darshan <darshanparmar200d@gmail.com>
2023-07-24 23:20:10 +0530

    bugs fixed

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -298,17 +298,15 @@ exports.getLeetcodeData = async (req, res) => {
             const response = await axios.post('https://leetcode.com/graphql', { query });
             const leetCodeData = response.data.data.matchedUser.submitStats.acSubmissionNum;
 
-                sendResponse(leetCodeData, res, 200);
-            } catch (error) {
-                console.error(error);
-                sendResponse('An error occurred while fetching data from LeetCode API', res, 500);
-            }
+            sendResponse(leetCodeData, res, 200);
+        } catch (error) {
+            console.error(error);
+            sendResponse('An error occurred while fetching data from LeetCode API', res, 500);
         }
-        else
-            sendResponse('handle does not exist', res, 404);
     }
     else
-        sendResponse('user does not exist', res, 404);
+        sendResponse('handle does not exist', res, 404);
+
 
 }
 
@@ -320,21 +318,18 @@ exports.getCodeforcesData = async (req, res) => {
             const codeforcesData = await processCodeforcesData(response.data.result);
             codeforcesData.handle = handle;
 
-                sendResponse(codeforcesData ,res,200);
+            sendResponse(codeforcesData, res, 200);
 
-            }
-            catch(error)
-            {
-                console.error(error.message);
-                sendResponse('An error occurred while fetching data from Codeforces API', res, error.status);
-            }
         }
-        else
-            sendResponse('handle does not exist', res, 404);
+        catch (error) {
+            console.error(error.message);
+            sendResponse('An error occurred while fetching data from Codeforces API', res, error.status);
+        }
     }
     else
-        sendResponse('user does not exist', res, 404);
-};
+        sendResponse('handle does not exist', res, 404);
+}
+
 
 exports.saveCode = async (req, res) => {
 
@@ -357,89 +352,88 @@ function processData(result) {
     const heatmap = {};
     let totalSub = 0;
     let years = 0;
-  
+
     for (var i = result.length - 1; i >= 0; i--) {
-      var sub = result[i];
-      var rating = sub.problem.rating === undefined ? 0 : sub.problem.rating;
-      var problemId = sub.problem.contestId + '-' + sub.problem.name + '-' + rating;
-      var problemIdprev = sub.problem.contestId - 1 + '-' + sub.problem.name + '-' + rating;
-      var problemIdnext = sub.problem.contestId + 1 + '-' + sub.problem.name + '-' + rating;
-  
-      if (problems[problemIdprev] !== undefined) {
-        if (problems[problemIdprev].solved === 0) {
-          problems[problemIdprev].attempts++;
+        var sub = result[i];
+        var rating = sub.problem.rating === undefined ? 0 : sub.problem.rating;
+        var problemId = sub.problem.contestId + '-' + sub.problem.name + '-' + rating;
+        var problemIdprev = sub.problem.contestId - 1 + '-' + sub.problem.name + '-' + rating;
+        var problemIdnext = sub.problem.contestId + 1 + '-' + sub.problem.name + '-' + rating;
+
+        if (problems[problemIdprev] !== undefined) {
+            if (problems[problemIdprev].solved === 0) {
+                problems[problemIdprev].attempts++;
+            }
+            problemId = problemIdprev;
+        } else if (problems[problemIdnext] !== undefined) {
+            if (problems[problemIdnext].solved === 0) {
+                problems[problemIdnext].attempts++;
+            }
+            problemId = problemIdnext;
+        } else if (problems[problemId] !== undefined) {
+            if (problems[problemId].solved === 0) {
+                problems[problemId].attempts++;
+            }
+        } else {
+            problems[problemId] = {
+                problemlink: sub.contestId + '-' + sub.problem.index,
+                attempts: 1,
+                solved: 0
+            };
         }
-        problemId = problemIdprev;
-      } else if (problems[problemIdnext] !== undefined) {
-        if (problems[problemIdnext].solved === 0) {
-          problems[problemIdnext].attempts++;
+
+        if (sub.verdict == 'OK') {
+            problems[problemId].solved++;
         }
-        problemId = problemIdnext;
-      } else if (problems[problemId] !== undefined) {
-        if (problems[problemId].solved === 0) {
-          problems[problemId].attempts++;
+
+        if (problems[problemId].solved === 1 && sub.verdict == 'OK') {
+            sub.problem.tags.forEach(function (t) {
+                if (tags[t] === undefined) tags[t] = 1;
+                else tags[t]++;
+            });
+
+            if (levels[sub.problem.index[0]] === undefined)
+                levels[sub.problem.index[0]] = 1;
+            else levels[sub.problem.index[0]]++;
+
+            if (sub.problem.rating) {
+                if (ratings[sub.problem.rating] === undefined) {
+                    ratings[sub.problem.rating] = 1;
+                } else {
+                    ratings[sub.problem.rating]++;
+                }
+            }
         }
-      } else {
-        problems[problemId] = {
-          problemlink: sub.contestId + '-' + sub.problem.index,
-          attempts: 1,
-          solved: 0
-        };
-      }
-  
-      if (sub.verdict == 'OK') {
-        problems[problemId].solved++;
-      }
-  
-      if (problems[problemId].solved === 1 && sub.verdict == 'OK') {
-        sub.problem.tags.forEach(function (t) {
-          if (tags[t] === undefined) tags[t] = 1;
-          else tags[t]++;
-        });
-  
-        if (levels[sub.problem.index[0]] === undefined)
-          levels[sub.problem.index[0]] = 1;
-        else levels[sub.problem.index[0]]++;
-  
-        if (sub.problem.rating) {
-          if (ratings[sub.problem.rating] === undefined) {
-            ratings[sub.problem.rating] = 1;
-          } else {
-            ratings[sub.problem.rating]++;
-          }
-        }
-      }
-  
-      if (verdicts[sub.verdict] === undefined) verdicts[sub.verdict] = 1;
-      else verdicts[sub.verdict]++;
-  
-      if (langs[sub.programmingLanguage] === undefined)
-        langs[sub.programmingLanguage] = 1;
-      else langs[sub.programmingLanguage]++;
-  
-      var date = new Date(sub.creationTimeSeconds * 1000);
-      date.setHours(0, 0, 0, 0);
-      if (heatmap[date.valueOf()] === undefined) heatmap[date.valueOf()] = 1;
-      else heatmap[date.valueOf()]++;
-      totalSub = result.length;
-  
-      years =
-        new Date(result[0].creationTimeSeconds * 1000).getYear() -
-        new Date(result[result.length - 1].creationTimeSeconds * 1000).getYear();
-      years = Math.abs(years) + 1;
+
+        if (verdicts[sub.verdict] === undefined) verdicts[sub.verdict] = 1;
+        else verdicts[sub.verdict]++;
+
+        if (langs[sub.programmingLanguage] === undefined)
+            langs[sub.programmingLanguage] = 1;
+        else langs[sub.programmingLanguage]++;
+
+        var date = new Date(sub.creationTimeSeconds * 1000);
+        date.setHours(0, 0, 0, 0);
+        if (heatmap[date.valueOf()] === undefined) heatmap[date.valueOf()] = 1;
+        else heatmap[date.valueOf()]++;
+        totalSub = result.length;
+
+        years =
+            new Date(result[0].creationTimeSeconds * 1000).getYear() -
+            new Date(result[result.length - 1].creationTimeSeconds * 1000).getYear();
+        years = Math.abs(years) + 1;
     }
-  
+
     // Return the processed data as an object
     return {
-      problems,
-      tags,
-      levels,
-      ratings,
-      verdicts,
-      langs,
-      heatmap,
-      totalSub,
-      years,
+        problems,
+        tags,
+        levels,
+        ratings,
+        verdicts,
+        langs,
+        heatmap,
+        totalSub,
+        years,
     };
-  }
-  
+}
